e1a216f0a096a33d2eecca106950b013bdc1b2f9
Marie-Charlotte <mariecharlotte.filippi@gmail.com>
2019-09-13 21:26:52 +0200

    Revert "update duration and startT wh noteon & noteoff"
    This reverts commit 6b893bb0f5ee1950720d4e6918011542888c2a68.

--- src/Track.js
+++ src/Track.js
@@ -194,14 +194,6 @@ class Track {
 	}
 
 	/**
-	 * Set the time of the first event in seconds
-	 * @type {Number}
-	 */
-	set startTime(value) {
-		this.startTime = value
-	}
-
-	/**
 	 * The time of the last event in seconds
 	 * @type {Number}
 	 */
@@ -212,14 +204,6 @@ class Track {
 		} else {
 			return 0
 		}
-	}
-
-	/**
-	 * Set the time of the last event in seconds
-	 * @type {Number}
-	 */
-	set duration(value) {
-		this.duration = value
 	}
 
 	/**
